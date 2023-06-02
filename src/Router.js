c06f73b093869e473316100f93965c80ebd71c71
Francis Mensah <117544815+Cazio88@users.noreply.github.com>
2023-06-02 14:40:21 -0700

    first commit

---
+++ src/Router.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import ServiceDetails from './components/ServiceDetails';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path= "/services" element={<Services />} />
+        <Route path="/servicedetails" element={<ServiceDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
